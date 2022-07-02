ac6c9519ae03a1e621af1c7ed17c0e1c8fc22804
saifit2787 <saifit27872@gmail.com>
2022-07-02 10:37:07 +0700

    create app/Http/Controller/EloquentController

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
+use App\Http\Controllers\ControllerUtama;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\LatihanController;
+use App\Http\Controllers\SalesOrderController;
+
+
+Route::get('/MorpTo', [LatihanController::class, 'index']);
 
 Route::get('/', function () {
     return view('welcome');
