ec01973d37e43d408a62ce5bce2fb0b7fa5735f5
saifit2787 <saifit2787@gmail.com>
2022-07-05 09:32:21 +0700

    merubah index nambah tabel

--- resources/views/Eloquent/index.blade.php
+++ resources/views/Eloquent/index.blade.php
@@ -8,6 +8,19 @@
         <h1> Halman Index </h1>
         <p>kita bisa menulis paragraf di sini</p>
 
+        <table>
+            <thead>
+                <tr> No</tr>
+                <tr> Nama</tr>
+                <tr> Alamat</tr>
+            </thead>
+            <tbody>
+                <td>1</td>
+                <td>Saiful Fitri</td>
+                <td>malang</td>
+            </tbody>
+        </table>
+
     
     </body>
 </html>
